0d30540748045c127c19f1c528f5f76545cc3e54
IAmMisterFrank <fsolleveld@outlook.com>
2020-04-11 13:02:22 +0200

    loaded in static messages

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -4,16 +4,26 @@ import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
+    // MARK: - @IBOutlets
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    // MARK: - Variables
+    var messages: [Message] = [
+        Message(sender: "123@@123.com", body: "Hello!"),
+        Message(sender: "a@b.com", body: "Hi."),
+        Message(sender: "123@123.com", body: "What's up?")
+    ]
+    
+    // MARK: - LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         title = K.appTitle
         navigationItem.hidesBackButton = true
-
     }
     
+    // MARK: - @IBActions
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
@@ -28,3 +38,17 @@ class ChatViewController: UIViewController {
     }
     
 }
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = "\(messages[indexPath.row].body)"
+        return cell
+    }
+}
+
+
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Frank Solleveld on 11/04/2020.
+//
+
+import Foundation
+import UIKit
+
+struct Message {
+    let sender: String // Sender: 123@123.com
+    let body: String // The message itself
+}
