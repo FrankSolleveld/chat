8550f6180e11ea95a1be36ad1949c99f1c1e667b
IAmMisterFrank <fsolleveld@outlook.com>
2020-04-11 11:29:26 +0200

    added firebase + registering users

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -1,12 +1,26 @@
-/
+
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            // MARK: - TODO: - iCloud Keychain
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let err = error {
+                    print(err.localizedDescription)
+                    self.errorLabel.text = err.localizedDescription
+                } else if let result = authResult {
+                    print("Authentication successfully with result: \(result)")
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
     
 }
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -2,14 +2,25 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
+    // MARK: - @IBOutlet
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        titleLabel.text = ""
+        var characterIndex = 0.0
+        let titleText = "FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * characterIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            characterIndex += 1
+        }
     }
     
-
 }
+
+
+
