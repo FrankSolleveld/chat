274143bf6dc8c193c047a523d39a8870d8c8ed60
IAmMisterFrank <fsolleveld@outlook.com>
2020-04-14 16:31:59 +0200

    Build 9 + custom App Icon

--- GeoChatTests/GeoChatTests.swift
+++ GeoChatTests/GeoChatTests.swift
@@ -7,27 +7,18 @@
 
 import XCTest
 import CoreLocation
+//@testable import GeoChat
 
 class GeoChatTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testSaveCoordinates(coordinate: CLLocationCoordinate2D) throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testHelloWorld() {
+        var helloWorld: String?
+        
+        XCTAssertNil(helloWorld)
+        
+        helloWorld = "Hello World"
+        
+        XCTAssertEqual(helloWorld, "Hello World")
     }
 
 }
